a12d4149d9801b03fc5e1eef09cbb3e844a31fc4
Dmitry Sychev <axiumone@gmail.com>
2020-06-03 17:38:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function increaseRankBy(n) {
   let children = secondList.children
 
     for (let i = 0, l = children.length; i < l; i++) {
-      return start += parseInt(children[i].innerHTML);
+      return start + i + n;
     }
 }
 
